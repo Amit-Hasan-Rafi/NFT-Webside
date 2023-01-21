21989857dc5d2aabea4b262ace1c9ad1fb9084f3
Amit Hasan Rafi <amithasanrafe1999@gmail.com>
2023-01-21 15:46:48 +0600

    header done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import "./Header.css"
 function Header() {
   return (
-    <div>Header</div>
+    <div className='header absolute-center cur-po'>
+      <span className="heading-gradient">NFT WEBSDIE</span>
+    </div>
   )
 }
 
