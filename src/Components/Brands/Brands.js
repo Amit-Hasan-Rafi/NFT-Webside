82817afd05c8a060cb93a153ed7aa56e5bb68d9d
Amit Hasan Rafi <amithasanrafe1999@gmail.com>
2023-01-22 15:41:54 +0600

    compays added

--- src/Components/Brands/Brands.js
+++ src/Components/Brands/Brands.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './Brands.css'
 function Brands() {
   return (
-    <div>Brands</div>
+    <div className='brands-div'>
+      <div className='brands-div-bg' ></div>
+      <div className='brands-section absolute-center'>
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/X2wRxdc/Wallet-Connect-logo-1.png' />
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/S0VwdQ2/Payoneer-logo.png' />
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/ZJXx27c/Walmart-logo.png' />
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/Jn34n6r/Metamask-logo.png' />
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/svTSgKT/580b57fcd9996e24bc43c530.png' />
+        <img alt='companys' className='c-imgs' src='https://i.ibb.co/vB3mk0D/Myntra-logo.png' />
+      </div>
+    </div>
   )
 }
 
